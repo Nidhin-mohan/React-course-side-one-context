2cd2424afc9c5abb9fbb6040d54f8cd8dc1e7e21
Nidhin-mohan <nidhinmohannidhin>
2022-10-24 10:21:19 +0530

    first commit

---
+++ src/problem.js
@@ -0,0 +1,31 @@
+// import React, { useState } from "react";
+
+// const GrandChild = (props) => {
+//   return (
+//     <>
+//       <h3>GrandChild :</h3>
+//       <Child brand={props.brand} />
+//     </>
+//   );
+// };
+
+// const Child = (props) => {
+//   return (
+//     <>
+//       <h2>Child: {props.brand} </h2>
+//     </>
+//   );
+// };
+
+// const App = () => {
+//   const [brandName] = useState("Amazone");
+//   return (
+//     <>
+//       <h1>Hello</h1>
+
+//       <GrandChild brand={brandName} />
+//     </>
+//   );
+// };
+
+// export default App;
